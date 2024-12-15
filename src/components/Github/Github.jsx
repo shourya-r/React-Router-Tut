1f4ed3072f8bdc27a6ec948b16e887f5b30de60d
shourya-r <rajputshourya7@gmail.com>
2024-12-15 12:50:14 +0530

    Added the github page and used loader functionality of router

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/torvalds")
+  //       .then((res) => res.json())
+  //       .then((res) => {
+  //         setData(res);
+  //       });
+  //   }, []);
+  const data = useLoaderData();
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Profile picture" width={300} />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/torvalds");
+  return response.json();
+};
