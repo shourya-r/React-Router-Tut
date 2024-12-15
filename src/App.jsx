e0bb6b32d7b781b8078e0c19e4b0f89abe60f1ac
shourya-r <rajputshourya7@gmail.com>
2024-12-15 12:15:08 +0530

    App.jsx not needed for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
 function App() {
   return <></>;
 }
